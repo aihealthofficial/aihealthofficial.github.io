4eb77140ba0c11a345598369144fa74a575daf36
AayanWasti <161695400+AayanWasti@users.noreply.github.com>
2024-12-09 00:13:24 -0500

    Create faq.js

---
+++ js/faq.js
@@ -0,0 +1,9 @@
+// Select all FAQ items
+const faqItems = document.querySelectorAll('.faq-item');
+
+// Add click event listener to each FAQ item
+document.querySelectorAll('.faq-item').forEach((item) => {
+  item.addEventListener('click', () => {
+    item.classList.toggle('active');
+  });
+});
